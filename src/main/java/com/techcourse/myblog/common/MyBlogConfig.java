8207682c4f150b3493a368b9b9a580c659b50232
andole87 <andole87@gmail.com>
2019-12-30 23:29:23 +0900

    feat(common): CORS Disable

--- src/main/java/com/techcourse/myblog/common/MyBlogConfig.java
+++ src/main/java/com/techcourse/myblog/common/MyBlogConfig.java
@@ -1,9 +1,22 @@
 package com.techcourse.myblog.common;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @Configuration
 public class MyBlogConfig {
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
 }
